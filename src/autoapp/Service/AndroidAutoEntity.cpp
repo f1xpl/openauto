bf2bde4cf2f180d9b0d34bdba00ef3a4e049db8b
Oper92 <48635119+Oper92@users.noreply.github.com>
2019-03-20 22:36:05 +0100

    Update AndroidAutoEntity.cpp

--- src/autoapp/Service/AndroidAutoEntity.cpp
+++ src/autoapp/Service/AndroidAutoEntity.cpp
@@ -272,7 +272,7 @@ void AndroidAutoEntity::sendPing()
     promise->then([]() {}, std::bind(&AndroidAutoEntity::onChannelError, this->shared_from_this(), std::placeholders::_1));
 
     aasdk::proto::messages::PingRequest request;
-	request.set_timestamp(1111111111); 
+	request.set_timestamp(11111); 
     controlServiceChannel_->sendPingRequest(request, std::move(promise));
 }
 
